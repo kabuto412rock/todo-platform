c1124bc015649c264419870ac359ea06eeb9ab2f
ZongKaKu <kabuto412rock@gmail.com>
2022-03-20 12:35:37 +0800

    feat: 後端_使用者操作待辦事項的API(更新、刪除)
    目前關於待辦事項的後端API已經進入穩定，可以開始接給前端去測試

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,6 +1,26 @@
 const asyncHandler = require("express-async-handler");
-const { status } = require("express/lib/response");
 const Todo = require("../models/todoModel");
+
+// @desc    Get user todo
+// @route   GET /api/todos/:id
+// @access  Private
+const getTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.findOne({
+    _id: req.params.todoId,
+    $or: [{ status: "public" }, { author: req.user.id }],
+  });
+
+  if (!todo) {
+    res.status(404);
+    throw new Error("Todo not found");
+  }
+  // if (todo.user.toString() !== req.user.id) {
+  //   res.status(401);
+  //   throw new Error("Not Authorized");
+  // }
+
+  res.status(200).json(todo);
+});
 
 // @desc    Get todos
 // @route   GET /api/todos/
@@ -38,7 +58,11 @@ const getTodos = asyncHandler(async (req, res) => {
       },
       $or: [{ status: "public" }, { author: req.user.id }],
     },
-    { page, limit }
+    {
+      page,
+      limit,
+      sort: { updatedAt: -1, title: 1 },
+    }
   );
 
   res.status(200).json(todos);
@@ -64,9 +88,61 @@ const createTodo = asyncHandler(async (req, res) => {
   res.status(200).json(todo);
 });
 
+// @desc    Uupdate todo
+// @route   PUT /api/todos/:id
+// @access  Private
+const updateTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.findById(req.params.todoId);
+
+  if (!todo) {
+    res.status(404);
+    throw new Error("Todo not found");
+  }
+
+  if (todo.author.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    req.params.todoId,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(200).json(updatedTodo);
+});
+
+// @desc    Delete todo
+// @route   Delete /api/todos/:id
+// @access  Private
+const deleteTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.findById(req.params.todoId);
+
+  if (!todo) {
+    res.status(404);
+    throw new Error("Todo not found");
+  }
+
+  if (todo.author.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  await todo.remove();
+  res.status(200).json({
+    success: true,
+    message: "Delete Todo Success",
+  });
+});
 module.exports = {
+  getTodo,
   getTodos,
   createTodo,
+  updateTodo,
+  deleteTodo,
 };
 
 // <http://localhost:3004/todos?_page=1&_limit=4>; rel="first",
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { getTodos, createTodo } = require("../controllers/todoController");
+const {
+  getTodo,
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todoController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTodos).post(protect, createTodo);
+router
+  .route("/:todoId")
+  .get(protect, getTodo)
+  .put(protect, updateTodo)
+  .delete(protect, deleteTodo);
 
 module.exports = router;
