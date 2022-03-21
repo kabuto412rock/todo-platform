36aa2a67771f4eb630d7c7ce87ca3226a0d4f35e
ZongKaKu <kabuto412rock@gmail.com>
2022-03-21 09:20:11 +0800

    feat: 等會繼續修改note的部份，目前還在實驗

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,9 +1,60 @@
+import { useState } from "react";
 import Container from "../components/Container";
 
 function Notes() {
+  const [notes, setNotes] = useState([
+    {
+      index: "1",
+      title: "標題123",
+      category: "面試",
+      createdAt: "2022-03-23",
+    },
+  ]);
+
+  const NoteSearchBar = () => (
+    <div className="w-full form-control">
+      <div className="input-group">
+        <input
+          type="search"
+          className="input input-bordered"
+          placeholder="標題..."
+        />
+        <button className="btn btn-info">搜尋</button>
+      </div>
+    </div>
+  );
+  const NoteItem = ({ note }) => (
+    <>
+      <tr>
+        <th>{note.index}</th>
+        <td>{note.category}</td>
+        <td>{note.title}</td>
+        <td>{note.createdAt}</td>
+      </tr>
+    </>
+  );
+
   return (
     <Container>
-      <div className="card w-full bg-base-100">123 New Notes</div>
+      <NoteSearchBar />
+
+      <div class="overflow-x-auto">
+        <table class="table-compact table w-full ">
+          <thead>
+            <tr>
+              <th></th>
+              <th>分類</th>
+              <th className="">標題</th>
+              <th>時間</th>
+            </tr>
+          </thead>
+          <tbody>
+            {notes.map((note) => (
+              <NoteItem note={note} key={note.index} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Container>
   );
 }
