ff14529fb9d4903c6ff7893461a28514eb03f36f
ZongKaKu <kabuto412rock@gmail.com>
2022-03-24 17:24:01 +0800

    feat: 當網址路徑不存在時，會導覽到指定的錯誤頁面
    錯誤頁面有設計5秒後，會自動重新轉址到首頁

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,23 +1,42 @@
 import { Link } from "react-router-dom";
 
-const NoteItem = ({ note, index }) => (
-  <>
-    <tr>
-      <th>{index}</th>
-      <td className="">{note.category}</td>
-      <td title={note.title}>
-        <Link
-          to={`/notes/${note._id}`}
-          className=" text-blue-400 hover:text-blue-700 active:text-blue-600"
-        >
-          <p className=" w-28 sm:w-auto   max-w-sm truncate ... break-all">
-            {note.title}
-          </p>
-        </Link>
-      </td>
-      <td>{note.updatedAt}</td>
-    </tr>
-  </>
-);
+const NoteItem = ({ note, index }) =>
+  note ? (
+    <>
+      <tr>
+        <th>{index}</th>
+        <td className="">{note.category}</td>
+        <td title={note.title}>
+          <Link
+            to={`/notes/${note._id}`}
+            className=" text-blue-400 hover:text-blue-700 active:text-blue-600"
+          >
+            <p className=" w-28 sm:w-auto   max-w-sm truncate ... break-all">
+              {note.title}
+            </p>
+          </Link>
+        </td>
+        <td>{note.updatedAt}</td>
+      </tr>
+    </>
+  ) : (
+    <>
+      <tr>
+        <th>{index}</th>
+        <td className="">分類</td>
+        <td>
+          <Link
+            to="/"
+            className=" text-blue-400 hover:text-blue-700 active:text-blue-600"
+          >
+            <p className=" w-28 sm:w-auto   max-w-sm truncate ... break-all">
+              標題...
+            </p>
+          </Link>
+        </td>
+        <td>更新時間...</td>
+      </tr>
+    </>
+  );
 
 export default NoteItem;
